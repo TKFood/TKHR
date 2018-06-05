d0d193eb96d8fa0f6774acb03264d1c2de106c1d
at3158 <tk290@tkfood.com.tw>
2018-06-05 15:39:42 +0800

    comboBox1_SelectedIndexChanged

--- TKHR/frmOT.Designer.cs
+++ TKHR/frmOT.Designer.cs
@@ -31,7 +31,31 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -47,6 +71,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
             this.tabPage1.Location = new System.Drawing.Point(4, 26);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -65,6 +90,185 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(970, 648);
+            this.splitContainer1.SplitterDistance = 165;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.LightGreen;
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel2.Controls.Add(this.dateTimePicker2, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dateTimePicker1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.numericUpDown1, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 4, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(966, 161);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Size = new System.Drawing.Size(970, 479);
+            this.splitContainer2.SplitterDistance = 724;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(720, 475);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "日期";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "~";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(123, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(363, 3);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(483, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "查詢";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "平日早出",
+            "平日延後",
+            "休息日",
+            "節日",
+            "假日"});
+            this.comboBox1.Location = new System.Drawing.Point(123, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "加班種類";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(243, 49);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(114, 27);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(483, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "查詢加班時數";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmOT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
@@ -76,6 +280,18 @@
             this.Name = "frmOT";
             this.Text = "frmOT";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -85,5 +301,18 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- TKHR/frmOT.cs
+++ TKHR/frmOT.cs
@@ -7,14 +7,166 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
 
 namespace TKHR
 {
     public partial class frmOT : Form
     {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string strFilePath;
+        OpenFileDialog file = new OpenFileDialog();
+        int result;
+        string NowDay;
+
         public frmOT()
         {
             InitializeComponent();
         }
+
+        #region FUNCTION
+        public void Search()
+        {
+            try
+            {
+
+                if (!string.IsNullOrEmpty(dateTimePicker1.Value.ToString()))
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+                    sbSql.AppendFormat(@" SELECT ");
+                    sbSql.AppendFormat(@" [AttendanceOTResult_Employee_EmployeeId].[Code] AS '工號'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_Employee_EmployeeId].[CnName] AS '姓名'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_CodeInfo_OvertimeKindId].[ScName] AS '班次'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult].[Hours] AS '加班時數'");
+                    sbSql.AppendFormat(@" ,CONVERT(nvarchar,[AttendanceOTResult].[Date],112) AS '加班日'");
+                    sbSql.AppendFormat(@" ,CONVERT(nvarchar,[AttendanceOTResult].[BeginDate],112) AS '加班開始日'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult].[BeginTime] AS '加班開始時間'");
+                    sbSql.AppendFormat(@" ,CONVERT(nvarchar,[AttendanceOTResult].[EndDate],112) AS '加班結束日'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult].[EndTime] AS '加班結束時間'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId].[Name] AS '部門'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId_Department_DirectDeptId].[Name] AS '課'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_Employee_EmployeeId_Job_JobId].[Name] AS '職稱'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_AttendanceOverTimePlan_AttendanceOTPlanId].[Name] AS '班別'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_AttendanceRank_AttendanceRankId].[Name] AS '加班別'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult_AttendanceType_AttendanceTypeId].[Name] AS '加班'");
+                    sbSql.AppendFormat(@" ,[AttendanceOTResult].[AttendanceOTResultId] AS 'ID'");
+                    sbSql.AppendFormat(@" FROM [HRMDB].dbo.[AttendanceOTResult] AS [AttendanceOTResult]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[Employee] AS [AttendanceOTResult_Employee_EmployeeId] ON [AttendanceOTResult].[EmployeeId]=[AttendanceOTResult_Employee_EmployeeId].[EmployeeId] ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[Department] AS [AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId] ON [AttendanceOTResult_Employee_EmployeeId].[DepartmentId]=[AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId].[DepartmentId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[Department] AS [AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId_Department_DirectDeptId] ON [AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId].[DirectDeptId]=[AttendanceOTResult_Employee_EmployeeId_Department_DepartmentId_Department_DirectDeptId].[DepartmentId] ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[Job] AS [AttendanceOTResult_Employee_EmployeeId_Job_JobId] ON [AttendanceOTResult_Employee_EmployeeId].[JobId]=[AttendanceOTResult_Employee_EmployeeId_Job_JobId].[JobId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[AttendanceRank] AS [AttendanceOTResult_AttendanceRank_AttendanceRankId] ON [AttendanceOTResult].[AttendanceRankId]=[AttendanceOTResult_AttendanceRank_AttendanceRankId].[AttendanceRankId] ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[AttendanceOTPlan] AS [AttendanceOTResult_AttendanceOverTimePlan_AttendanceOTPlanId] ON [AttendanceOTResult].[AttendanceOTPlanId]=[AttendanceOTResult_AttendanceOverTimePlan_AttendanceOTPlanId].[AttendanceOverTimePlanId] ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[AttendanceType] AS [AttendanceOTResult_AttendanceType_AttendanceTypeId] ON [AttendanceOTResult].[AttendanceTypeId]=[AttendanceOTResult_AttendanceType_AttendanceTypeId].[AttendanceTypeId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[CodeInfo] AS [AttendanceOTResult_CodeInfo_OvertimeKindId] ON [AttendanceOTResult].[OvertimeKindId]=[AttendanceOTResult_CodeInfo_OvertimeKindId].[CodeInfoId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[Employee] AS [AttendanceOTResult_Employee_ApproveEmployeeId] ON [AttendanceOTResult].[ApproveEmployeeId]=[AttendanceOTResult_Employee_ApproveEmployeeId].[EmployeeId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[CodeInfo] AS [AttendanceOTResult_CodeInfo_ApproveResultId] ON [AttendanceOTResult].[ApproveResultId]=[AttendanceOTResult_CodeInfo_ApproveResultId].[CodeInfoId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[User] AS [AttendanceOTResult_User_CreateBy] ON [AttendanceOTResult].[CreateBy]=[AttendanceOTResult_User_CreateBy].[UserId]  ");
+                    sbSql.AppendFormat(@" LEFT  JOIN [HRMDB].dbo.[User] AS [AttendanceOTResult_User_LastModifiedBy] ON [AttendanceOTResult].[LastModifiedBy]=[AttendanceOTResult_User_LastModifiedBy].[UserId]  ");
+                    sbSql.AppendFormat(@" WHERE [AttendanceOTResult].[Date]>='{0}' AND [AttendanceOTResult].[Date]<='{0}'",dateTimePicker1.Value.ToString("yyyyMMdd"), dateTimePicker2.Value.ToString("yyyyMMdd"));
+                    sbSql.AppendFormat(@" AND [AttendanceOTResult_CodeInfo_OvertimeKindId].[ScName]='{0}'",comboBox1.Text.ToString());
+                    sbSql.AppendFormat(@" ORDER BY [AttendanceOTResult].[Date],[AttendanceOTResult].[AttendanceOTResultId]");                    
+                    sbSql.AppendFormat(@" ");
+                    sbSql.AppendFormat(@" ");
+
+
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds.Clear();
+                    adapter.Fill(ds, "TEMPds");
+                    sqlConn.Close();
+
+
+                    if (ds.Tables["TEMPds"].Rows.Count == 0)
+                    {
+
+                    }
+                    else
+                    {
+
+                        dataGridView1.DataSource = ds.Tables["TEMPds"];
+                        dataGridView1.AutoResizeColumns();
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Text.ToString().Equals("平日早出"))
+            {
+                numericUpDown1.Value = 4;
+            }
+            else if (comboBox1.Text.ToString().Equals("平日延後"))
+            {
+                numericUpDown1.Value = 4;
+            }
+            else if (comboBox1.Text.ToString().Equals("休息日"))
+            {
+                numericUpDown1.Value = 12;
+            }
+            else if (comboBox1.Text.ToString().Equals("節日"))
+            {
+                numericUpDown1.Value = 12;
+            }
+            else if (comboBox1.Text.ToString().Equals("假日"))
+            {
+                numericUpDown1.Value = 0;
+            }
+        }
+
+        #endregion
+
+        #region BUTTON
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        #endregion
+
+       
     }
 }
