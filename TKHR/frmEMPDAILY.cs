cd5ad198479d07dd030390e3cdd0aa3e6e2e6bee
at3158 <tk290@tkfood.com.tw>
2020-04-20 13:07:49 +0800

    add frmEMPDAILY

---
+++ TKHR/frmEMPDAILY.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.Util;
+using System.Reflection;
+using System.Threading;
+
+namespace TKHR
+{
+    public partial class frmEMPDAILY : Form
+    {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlDataAdapter adapter1 = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder1 = new SqlCommandBuilder();
+
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataSet ds1 = new DataSet();
+
+        public frmEMPDAILY()
+        {
+            InitializeComponent();
+        }
+
+        #region FUNCTION
+
+        #endregion
+        public void OPENFILE()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "xls files (*.*)|*.xls";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dialog.FileName);
+            }
+        }
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OPENFILE();
+        }
+        #endregion
+    }
+}
