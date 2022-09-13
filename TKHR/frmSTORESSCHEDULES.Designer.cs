4ae27895275ebc442afb9a3b90a03d1642b1aba2
at3158 <tk290@tkfood.com.tw>
2022-09-13 14:10:21 +0800

    RESETSTORESSCHEDULESNEWBREAKDATES

--- TKHR/frmSTORESSCHEDULES.Designer.cs
+++ TKHR/frmSTORESSCHEDULES.Designer.cs
@@ -42,6 +42,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -142,16 +143,17 @@
             // 
             this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGreen;
             this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 84F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.button2, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 3, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -168,9 +170,9 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(123, 3);
+            this.button1.Location = new System.Drawing.Point(87, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.Size = new System.Drawing.Size(121, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "查詢設定";
             this.button1.UseVisualStyleBackColor = true;
@@ -194,7 +196,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.Size = new System.Drawing.Size(78, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "開始日";
             // 
@@ -203,17 +205,17 @@
             this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePicker1.CustomFormat = "yyyy/MM/dd";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(123, 86);
+            this.dateTimePicker1.Location = new System.Drawing.Point(87, 86);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(114, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 27);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(123, 163);
+            this.button2.Location = new System.Drawing.Point(87, 163);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 34);
+            this.button2.Size = new System.Drawing.Size(121, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "排休";
             this.button2.UseVisualStyleBackColor = true;
@@ -225,17 +227,28 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(3, 132);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.Size = new System.Drawing.Size(78, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "排休次數";
             // 
             // textBox1
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(123, 126);
+            this.textBox1.Location = new System.Drawing.Point(87, 126);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(114, 27);
+            this.textBox1.Size = new System.Drawing.Size(121, 27);
             this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(274, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "重設預計到期日";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // frmSTORESSCHEDULES
             // 
@@ -280,5 +293,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- TKHR/frmSTORESSCHEDULES.cs
+++ TKHR/frmSTORESSCHEDULES.cs
@@ -252,19 +252,342 @@ namespace TKHR
             }
         }
         /// <summary>
+        /// 新增排休到 STORESSCHEDULESRESULTS
+        /// </summary>
+        /// <param name="SEQ"></param>
+        public void ADDSTORESSCHEDULESRESULTS(string SEQ)
+        {
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+
+
+                sbSql.AppendFormat(@"  
+
+                                    INSERT INTO [TKHR].[dbo].[STORESSCHEDULESRESULTS]
+                                    (
+                                    [SEQ]
+                                    ,[NAMES]
+                                    ,[NOWDATES]
+                                    ,[NEWBREAKDATES]
+                                    ,[DAYS]
+                                    )
+
+                                    SELECT 
+                                    {0}
+                                    ,[NAMES]
+                                    ,[NOWDATES]
+                                    ,[NEWBREAKDATES]
+                                    ,DATEDIFF(DAY,[NOWDATES],[NEWBREAKDATES]) AS 'DAYS'
+                                    FROM [TKHR].[dbo].[STORESSCHEDULES]
+                                    ORDER BY DATEDIFF(DAY,[NOWDATES],[NEWBREAKDATES])
+
+                                    ", SEQ);
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+        /// <summary>
+        /// 清空STORESSCHEDULESRESULTS
+        /// </summary>
+        public void DELETESTORESSCHEDULESRESULTS()
+        {
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+
+
+                sbSql.AppendFormat(@"  
+                                    DELETE [TKHR].[dbo].[STORESSCHEDULESRESULTS]
+
+                                    ");
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+        /// <summary>
         /// 排休，依 開始日、排休次數
         /// </summary>
-        public void ADDSTORESSCHEDULESRESULTS(int COUNTS)
+        public void SETSTORESSCHEDULESRESULTS(int COUNTS)
         {
             DateTime SDT = dateTimePicker1.Value;
+            int NUMS = SEARCHSTORESSCHEDULES();
 
-            for (int i = 1; i < COUNTS; i++)
+            DELETESTORESSCHEDULESRESULTS();
+
+            for (int i = 1; i <= COUNTS; i++)
             {
                 UPDATESTORESSCHEDULESNOWDATES(SDT.ToString("yyyyMMdd"));
                 UPDATESTORESSCHEDULESNEWBREAKDATES();
+
+                ADDSTORESSCHEDULESRESULTS(i.ToString());
+
+                SDT = SDT.AddDays(7* NUMS);
             }
 
             //MessageBox.Show(COUNTS+" "+ SDT.ToString("yyyyMMdd"));
+        }
+
+        public void RESETSTORESSCHEDULESNEWBREAKDATES()
+        {
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+
+
+                sbSql.AppendFormat(@"  
+                                    UPDATE [TKHR].[dbo].[STORESSCHEDULES]
+                                    SET [NEWBREAKDATES]=[ORIBREAKDATES]
+
+                                    ");
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
+        public int SEARCHSTORESSCHEDULES()
+        {
+            int NUMS = 0;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+
+            SqlTransaction tran;
+            SqlCommand cmd = new SqlCommand();
+            DataSet ds = new DataSet();
+
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                StringBuilder sbSql = new StringBuilder();
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+                ds.Clear();
+
+              
+                sbSql.AppendFormat(@"  
+                                    SELECT ISNULL(COUNT(*),0)  AS COUNTS FROM [TKHR].[dbo].[STORESSCHEDULES]
+
+                                    ");
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds.Clear();
+                adapter.Fill(ds, "TEMPds");
+                sqlConn.Close();
+
+
+                if (ds.Tables["TEMPds"].Rows.Count > 0)
+                {
+                    return Convert.ToInt32(ds.Tables["TEMPds"].Rows[0]["COUNTS"].ToString());
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
+            catch
+            {
+                return 0;
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
+        public void SETFASTREPORT()
+        {
+            StringBuilder SQL = new StringBuilder();
+            report1 = new Report();
+
+            report1.Load(@"REPORT\觀光排休表.frx");
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
+
+            //report1.Dictionary.Connections[0].ConnectionString = "server=192.168.1.105;database=TKPUR;uid=sa;pwd=dsc";
+
+            TableDataSource Table = report1.GetDataSource("Table") as TableDataSource;
+
+            SQL = SETFASETSQL();
+
+            Table.SelectCommand = SQL.ToString(); ;
+
+            report1.Preview = previewControl1;
+            report1.Show();
+
+        }
+
+        public StringBuilder SETFASETSQL()
+        {
+            StringBuilder FASTSQL = new StringBuilder();
+            StringBuilder STRQUERY = new StringBuilder();
+
+          
+
+            FASTSQL.AppendFormat(@"      
+                                SELECT 
+                                [ID]
+                                ,[SEQ] AS '排休次數'
+                                ,[NAMES] AS '姓名'
+                                ,[NOWDATES] AS '排休起算日'
+                                ,[NEWBREAKDATES] AS '預計到期日'
+                                ,[DAYS] AS '差異天數'
+                                FROM [TKHR].[dbo].[STORESSCHEDULESRESULTS]
+                                ORDER BY [ID]
+                                ");
+
+            return FASTSQL;
         }
 
         #endregion
@@ -283,7 +606,9 @@ namespace TKHR
 
             if (result)
             {
-                ADDSTORESSCHEDULESRESULTS(n);
+                SETSTORESSCHEDULESRESULTS(n);
+
+                SETFASTREPORT();
                 MessageBox.Show("完成");
             }
             else
@@ -295,6 +620,12 @@ namespace TKHR
 
             
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RESETSTORESSCHEDULESNEWBREAKDATES();
+        }
+
         #endregion
 
 
