2b00bd6c0f09c17b9694382df7f1642258529298
at3158 <tk290@tkfood.com.tw>
2022-09-22 13:54:17 +0800

    ADDToUOF_TB_EIP_PRIV_MESS

--- TKHR/frmUOF_TB_EIP_PRIV_MESS.cs
+++ TKHR/frmUOF_TB_EIP_PRIV_MESS.cs
@@ -157,8 +157,155 @@ namespace TKHR
         }
 
         public void ADDToUOF_TB_EIP_PRIV_MESS(string MESSAGE_TO, string MESSAGE_FROM, string CONTENT)
-        { 
+        {
+            Guid MESSAGE_GUID = Guid.NewGuid();
 
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbUOF"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+                
+                sbSql.AppendFormat(@" 
+
+INSERT [UOF].dbo.TB_EIP_PRIV_MESS
+( 
+MESSAGE_GUID
+, TOPIC
+, MESSAGE_CONTENT
+, MESSAGE_TO
+, MESSAGE_FROM
+, CREATE_TIME
+, FROM_DELETED
+, TO_DELETED
+, FILE_GROUP_ID
+, MASTER_GUID 
+) 
+VALUES 
+( 
+N'14BD9836-EFD6-4E0F-88F0-F8E2B418F3AA'
+, N'TEST'
+, N'TEST'
+, N'b6f50a95-17ec-47f2-b842-4ad12512b431'
+, N'b6f50a95-17ec-47f2-b842-4ad12512b431'
+,'2022/9/22 10:00:00'
+, 0
+, 0
+, N''
+, N'366BA6A2-120B-4595-9994-7E017A12C359' 
+)
+
+
+
+INSERT [UOF].dbo.TB_EIP_PRIV_MESS_MASTER
+( 
+MASTER_GUID
+, TOPIC
+, MESSAGE_FROM
+, MESSAGE_TO
+,SENDER_TIME
+, CREATOR
+, CREATE_FROM
+, CREATE_DATE
+, MODIFIER
+, MODIFY_FROM
+, MODIFY_DATE
+) 
+VALUES 
+(  
+N'14BD9836-EFD6-4E0F-88F0-F8E2B418F3AA'
+,N'TEST'
+,N'366BA6A2-120B-4595-9994-7E017A12C359'
+,N'<UserSet><Element type=""user""><userId>b6f50a95-17ec-47f2-b842-4ad12512b431</userId></Element></UserSet>'
+, '2022/9/22 10:00:00' 
+,N'366BA6A2-120B-4595-9994-7E017A12C359' 
+,N'192.168.1.57' 
+,'2022/9/22 10:00:00' 
+,N'366BA6A2-120B-4595-9994-7E017A12C359' 
+,N'192.168.1.57' 
+,'2022/9/22 10:00:00'
+)
+
+
+
+INSERT TB_EIP_PUSH_QUEUE
+( 
+[NOTIFY_ID]
+, [USER_GUID]
+, [DESCRIPTION]
+,[TITLE]
+, [DISPLAY_NUMBER]
+, [MODULE_NAME]
+, [MODULE_KEY]
+,[CREATOR]
+, [CREATE_FROM]
+, [CREATE_DATE]
+, [MODIFIER]
+, [MODIFY_FROM]
+, [MODIFY_DATE]
+)
+VALUES
+(
+N'ec997d38-a105-495d-b560-0477eb130c61'
+, N'b6f50a95-17ec-47f2-b842-4ad12512b431'
+, N'',
+N'TEST'
+, 1,
+N'PrivateMessage'
+, N'PrivateMessage?id=14BD9836-EFD6-4E0F-88F0-F8E2B418F3AA'
+,N'b6f50a95-17ec-47f2-b842-4ad12512b431'
+, N'192.168.1.103'
+, '2022/9/22 10:00:00'
+, N'b6f50a95-17ec-47f2-b842-4ad12512b431'
+, N'192.168.1.103'
+, '2022/9/22 10:00:00'
+)
+
+                                    ");
+
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
 
         }
 
